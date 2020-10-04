0c083d80b02cf82b4a50c322234342af8e1276e4
Nishi Anand <nishianand@Nishis-MacBook-Air.local>
2020-10-04 23:06:36 +0530

    first commit

---
+++ assignment.py
@@ -0,0 +1,58 @@
+from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+
+
+def getQuery():
+    query = input('Enter the book_url:' )
+    queryone= query.split("-")[0]
+    bookID=queryone.rsplit('/',1)[1]
+    metadataURL = urlopen(
+        f'https://www.goodreads.com/book/show/{bookID}?format=xml&key=mbNAjKhzOfh6FwOXAeBrg')
+    return metadataURL
+
+
+def get_book_details(metadataURL):
+    tree2 = ET.parse(metadataURL)
+    root2 = tree2.getroot()
+    author = []
+    title = None
+    average_rating=None
+    ratings_count=None
+    num_pages=None
+    coverImageURL = None
+    publication_year = None
+    
+    for t in root2.findall('./book/work/original_title'):
+        title = t.text
+    for av in root2.findall('./book/average_rating'):
+        average_rating = av.text
+    for rc in root2.findall('./book/ratings_count'):
+        ratings_count = rc.text
+    for num in root2.findall('./book/num_pages'):
+        num_pages = num.text
+    for a in root2.findall('./book/authors/author/name'):
+        author.append(a.text)
+    for i in root2.findall('./book/image_url'):
+        coverImageURL = i.text
+    for y in root2.findall('./book/work/original_publication_year'):
+        publication_year = y.text 
+    
+
+    metadataDict = {
+        'title': title,
+        'average_rating':average_rating,
+        'ratings_count':ratings_count,
+        'num_pages':num_pages, 
+        'image_url': coverImageURL,
+        'publication_year': publication_year,
+        'author': ', '.join(author),
+        
+    }
+    return metadataDict
+
+
+if __name__ == "__main__":
+    metadataURL = getQuery()
+    details = get_book_details(metadataURL)
+    print(details)
+    
---
+++ tests.py
@@ -0,0 +1,22 @@
+import requests
+
+class BookDetailTestView():
+    def setUp(self):
+       
+        self.valid_payload = {
+            'title':'A Song of Ice and Fire',
+            'average_rating':'4.57',
+            'ratings_count':'27741',
+            'num_pages':'5216',
+            'author':'George R.R. Martin',
+            'pages':'5216',
+            'image_url':'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1339340118l/12177850._SX98_.jpg',
+            'year':'2000'
+
+        }
+       
+    def test_url_get(self):
+        response = requests.get('https://www.goodreads.com/book/show/12177850-a-song-of-ice-and-fire')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+
